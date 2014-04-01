c22035612d26c2a8ec27f83d3b6325a4ec05b6dc
Jan Skalny <jan@skalny.sk>
2014-04-01 23:27:32 +0200

    added path from inetrnet to bss and modified API

--- ryu/app/experimenter_switch.py
+++ ryu/app/experimenter_switch.py
@@ -10,22 +10,39 @@ from ryu.ofproto import ofproto_parser
 from ryu.ofproto import inet
 import struct
 import sys 
+import socket
 
-VGSN_PORT = 2
-TUNNEL_PORT = 3
+BSS_PHY_PORT = 1
+VGSN_PHY_PORT = 2
+TUNNEL_PHY_PORT = 3
 
 GPRS_SDN_EXPERIMENTER = int("0x42", 16)
 OF_GPRS_TABLE = 2
 
+#TODO: kazdy PDP kontext patri nejakej bss-ke
+# mali by sme si spravit:
+# 1) mechanizmus na pripojenie bss-ky. controller musi sniffovat komunikaciu 
+#    medzi BSS a VGSN aby zistil, ze sa pripojila nova BSS. z komunikacie musi 
+#    ziskat IP adresu BSS a VGSN a zdrojovy a cielovy port BSS a VGSN
+#    XXX: casom sa mozu porty zmenit, ked restartujeme service na BSS
+# 2) zoznam pripojenych bss-iek
+# 3) v pdp-kontexte sa odkazovat na BSS-ku ku ktorej patri...
+BSS_IP="1.2.3.4"
+BSS_PORT=1234
+VGSN_IP="14.13.12.11"
+VGSN_PORT=23000
 
 class PDPContext:
-    def __init__(self, bvci, tlli, sapi, nsapi, tunnel):
+    def __init__(self, bvci, tlli, sapi, nsapi, tunnel_port, tunnel_internet, tunnel_bss, ip):
         self.bvci = bvci
         self.tlli = tlli
         self.sapi = sapi
         self.nsapi = nsapi
         #TODO: QoS a tunnel treba premysliet
-        self.tunnel = tunnel
+        self.ip = ip
+        self.tunnel_port = tunnel_port
+        self.tunnel_internet = tunnel_internet
+        self.tunnel_bss = tunnel_bss
 
 
 class GPRSControll(app_manager.RyuApp):
@@ -38,7 +55,7 @@ class GPRSControll(app_manager.RyuApp):
         # TODO: o tom ake kontexty mame (a na ktorom datapathe) sa budeme 
         # dozvedat dynamicky cez REST
         # zatial, kazdy datapath ma vsetky kontexty
-        self.active_contexts.append( PDPContext(bvci=2, tlli=0xc5a4aeea, sapi=3, nsapi=5, tunnel=0) )
+        self.active_contexts.append( PDPContext(bvci=2, tlli=0xc5a4aeea, sapi=3, nsapi=5, tunnel_port=TUNNEL_PHY_PORT, tunnel_internet='00:00:00:00:00:01', tunnel_bss='00:00:00:00:00:02', ip='10.10.10.10') )
 
     def add_pdp_context(self, dp, pdp, tunnel):
         """ Add new flow rule to redirect user traffic into tunnel interface.
@@ -57,14 +74,39 @@ class GPRSControll(app_manager.RyuApp):
 
         # pouzivatelsky packet s danymi parametrami tlacime do dodaneho tunelu
         match = parser.OFPMatch(
+                        in_port=BSS_PHY_PORT,
                         ns_type=0, 
                         ns_bvci=pdp.bvci,
                         bssgp_tlli=pdp.tlli,
                         llc_sapi=pdp.sapi,
                         sndcp_nsapi=pdp.nsapi)
-        actions = [ GPRSActionPopGPRSNS(), GPRSActionPopUDP(), GPRSActionPopIP(), parser.OFPActionOutput(port=tunnel) ]
+        actions = [ GPRSActionPopGPRSNS(), GPRSActionPopUDPIP() , parser.OFPActionOutput(port=tunnel) ]
         inst = [ parser.OFPInstructionActions(ofp.OFPIT_APPLY_ACTIONS, actions) ]
         req = parser.OFPFlowMod(datapath=dp, table_id=OF_GPRS_TABLE, priority=10, match=match, instructions=inst);
+        dp.send_msg(req)
+
+		# v opacnom smere nam vypadavaju z tunelu nejake packety.. balime ich do gprsns a posielame na bss
+        # TODO: (mato) prepisat tento match, malo by sa matchovat:
+        #  - zdrojova a cielova mac adresa tunela
+        #  - cielova IP adresa (podla adresy ktoru ma priradeny PDP kontext)
+        match = parser.OFPMatch( 
+                in_port=TUNNEL_PHY_PORT,
+                eth_type=0x0800, 
+                eth_src=pdp.tunnel_internet,
+                eth_dst=pdp.tunnel_bss,
+                ipv4_dst=pdp.ip)
+        # TODO: (mato)
+        # adresa 10.11.12.13  adresa VGSN
+        # adresa 20.21.22.23 je adresa BSS 
+        # sp by mal byt port ktory sa nejako dozvieme (z prveho pripojenia BSS na VGSN)
+        # dp by mal byt port na ktorom pocuva BSS, tiez sa ho dozvieme z prveho pripojenia BSS
+        #XXX: kontrolny traffic z BSS na VGSN by sa mal asi posielat aj do controlleru, na analyzu cisiel portov?? minimalne na zaciatku
+        actions = [
+            GPRSActionPushGPRSNS( bvci=pdp.bvci, tlli=pdp.tlli, sapi=pdp.sapi, nsapi=pdp.nsapi),
+            GPRSActionPushUDPIP( sa=VGSN_IP, da=BSS_IP, sp=VGSN_PORT, dp=BSS_PORT),
+            parser.OFPActionOutput( port=BSS_PHY_PORT ) ] 
+        inst = [ parser.OFPInstructionActions(ofp.OFPIT_APPLY_ACTIONS, actions) ]
+        req = parser.OFPFlowMod(datapath=dp, table_id=OF_GPRS_TABLE, priority=11, match=match, instructions=inst);
         dp.send_msg(req)
 
     def on_inner_dp_join(self, dp):
@@ -85,9 +127,9 @@ class GPRSControll(app_manager.RyuApp):
         ##########################
         # hlavna flow tabulka (0)
 
-        # UDP 23000 je GPRS-NS
+        # UDP 23000 je GPRS-NS 
         inst = [ parser.OFPInstructionGotoTable(OF_GPRS_TABLE) ]
-        match = parser.OFPMatch(eth_type=0x0800,ip_proto=inet.IPPROTO_UDP, udp_dst=23000)
+        match = parser.OFPMatch(eth_type=0x0800,ip_proto=inet.IPPROTO_UDP, udp_dst=VGSN_PORT)
         req = parser.OFPFlowMod(datapath=dp, priority=1, match=match, instructions=inst)
         dp.send_msg(req)
 
@@ -103,7 +145,7 @@ class GPRSControll(app_manager.RyuApp):
         
         # ak je to prvy SNDCP fragment packetu s viac ako jednym fragmentom, ICMP a DROP
         match = parser.OFPMatch( sndcp_first_segment=1, sndcp_more_segments=1 )
-        actions = [ GPRSActionHello() ]
+        actions = [ ] #TODO: ICMP a drop
         inst = [ parser.OFPInstructionActions(ofp.OFPIT_APPLY_ACTIONS, actions) ]
         req = parser.OFPFlowMod(datapath=dp, table_id=OF_GPRS_TABLE, priority=20, match=match, instructions=inst)
         dp.send_msg(req)
@@ -112,7 +154,7 @@ class GPRSControll(app_manager.RyuApp):
         #XXX: pravidla pridava funkcia add_pdp_context
         #TODO: pridat vsetky aktivne PDP kontexty
         for pdp in self.active_contexts:
-            self.add_pdp_context(dp, pdp, TUNNEL_PORT)
+            self.add_pdp_context(dp, pdp, TUNNEL_PHY_PORT)
 
         # ak to ma sndcp, su to user data neznameho PDP kontextu - drop
         match = parser.OFPMatch( sndcp_first_segment=1 )
@@ -122,7 +164,7 @@ class GPRSControll(app_manager.RyuApp):
         dp.send_msg(req)
 
         # vsetko ostatne je signalizacia - tlacime do vGSN
-        actions = [ parser.OFPActionOutput(VGSN_PORT) ]
+        actions = [ parser.OFPActionOutput(VGSN_PHY_PORT) ]
         inst = [ parser.OFPInstructionActions(ofp.OFPIT_APPLY_ACTIONS, actions) ]
         req = parser.OFPFlowMod(datapath=dp, table_id=OF_GPRS_TABLE, priority=0, instructions=inst)
         dp.send_msg(req)
@@ -159,49 +201,28 @@ class GPRSActionPushGPRSNS(GPRSAction):
     def serialize(self, buf, offset):
         """ Serialize PushGPRSNS action into buffer. """
         super(GPRSActionPushGPRSNS,self).serialize(buf, offset)
-        ofproto_parser.msg_pack_into("!HHIBBxxxxxx", buf, offset+8, 
-                self.subtype, self.bvci, self.tlli, self.sapi, self.nsapi)
+        ofproto_parser.msg_pack_into("!HxxIHBBxxxx", buf, offset+8, 
+                self.subtype, self.tlli, self.bvci, self.sapi, self.nsapi)
 
 class GPRSActionPopGPRSNS(GPRSAction):
     def __init__(self):
         super(GPRSActionPopGPRSNS,self).__init__(0x0002)
 
-class GPRSActionPushIP(GPRSAction):
-    def __init__(self, sa, da):
-        super(GPRSActionPushIP,self).__init__(0x0003)
+class GPRSActionPushUDPIP(GPRSAction):
+    def __init__(self, dp, sp, da, sa):
+        super(GPRSActionPushUDPIP,self).__init__(0x0003)
         self.len = 24
-        self.sa = sa
-        self.da = da
-
-    def serialize(self, buf, offset):
-        """ Serialize PushIP action into buffer. """
-        super(GPRSActionPushIP,self).serialize(buf, offset)
-        ofproto_parser.msg_pack_into("!HxxIIxxxx", buf, offset+8,
-                self.subtype, self.sa, self.da)
-
-class GPRSActionPopIP(GPRSAction):
-    def __init__(self):
-        super(GPRSActionPopIP,self).__init__(0x0004)
-
-class GPRSActionPushUDP(GPRSAction):
-    def __init__(self, sp, dp):
-        super(GPRSActionPushUDP,self).__init__(0x0005)
-        self.len = 16
         self.sp = sp
         self.dp = dp
+        self.da = socket.inet_aton(da)
+        self.sa = socket.inet_aton(sa)
 
     def serialize(self, buf, offset):
-        """ Serialize PushUDP action into buffer. """
-        super(GPRSActionPushUDP,self).serialize(buf, offset)
-        ofproto_parser.msg_pack_into("!HHHxx", buf, offset+8,
-                self.subtype, self.sa, self.da)
+        """ Serialize PushUDPIP action into buffer. """
+        super(GPRSActionPushUDPIP,self).serialize(buf, offset)
+        ofproto_parser.msg_pack_into("!Hxx4s4sHH", buf, offset+8, 
+                self.subtype, self.da, self.sa, self.dp, self.sp)
 
-class GPRSActionPopUDP(GPRSAction):
+class GPRSActionPopUDPIP(GPRSAction):
     def __init__(self):
-        super(GPRSActionPopUDP,self).__init__(0x0006)
-
-class GPRSActionHello(GPRSAction):
-    def __init__(self):
-        super(GPRSActionHello,self).__init__(0x0100)
-
-#    gprs_subtype = {'pushGPRSNS': '0x1', 'popGPRSNS': '0x2', 'pushIP': '0x3' , 'popIP': '0x4', 'pushUDP': '0x5', 'popUDP': '0x6', 'hello': '0x0100'}
+        super(GPRSActionPopUDPIP,self).__init__(0x0004)
