38b277c278673bc22509c12decef529426902fcc
kalcok <martin.kalcok@gmail.com>
2014-04-10 14:04:35 +0200

    Now working both ways :)

--- ryu/app/experimenter_switch_in_progres.py
+++ ryu/app/experimenter_switch_in_progres.py
@@ -101,7 +101,7 @@ class topology():
     def get_tunnel(self, fwID1, fwID2, tunnelID, mirrorID):
        hopy = nx.shortest_path(self.DynamicGraph, fwID1, fwID2)
        path = []
-       for k in hopy[0:-1]:
+       for k in hopy[1:-1]:
           path.append(node(k,self.DynamicGraph[k][hopy[hopy.index(k)+1]]['interf']))
        t = tunnels(tunnelID, mirrorID, path)
        return(t)
@@ -116,6 +116,7 @@ VGSN_PHY_PORT = 2
 GPRS_SDN_EXPERIMENTER = int("0x42", 16)
 OF_GPRS_TABLE = 2
 
+topo = topology()
 #TODO: kazdy PDP kontext patri nejakej bss-ke
 # mali by sme si spravit:
 # 1) mechanizmus na pripojenie bss-ky. controller musi sniffovat komunikaciu 
@@ -155,13 +156,11 @@ class PDPContext:
 #
 # Example body:
 #
+#{'start' : '0', 'end' : '2', 'bvci' : '2', 'tlli' : '0xc5a4aeea', 'sapi' : '3', 'nsapi' : '5', 'clientIP' : '172.20.0.2', 'mirror' : 'yes'}
 #
-#{'start' : '0x0', 'end' : '0x2', 'bvci' : '2', 'tlli' : '0xc5a4aeea', 'sapi' : '3', 'nsapi' : '5', 'mirror' : 'yes'}
-#
-# start = datapath ID of first forwarder in tunnel
-# end = datapath ID of last forwarder in tunnel
-# condition = dictionary of supported match rules
-#     supperted match fields = eth_type, ipv4_src, ipv4_dst, eth_src, eth_dst
+# start = datapath ID of first forwarder (or pseudo-node, BSS, vGSN etc.) in tunnel
+# end = datapath ID of last forwarder (or pseudo-node like APN) in tunnel
+# bvc, tlli, sapi, nsapi = parameters of PDP Context
 # mirror = if 'yes' then tunnel will be created in backward dirrection too
 
 active_contexts = []
@@ -192,69 +191,18 @@ class GPRSControll(app_manager.RyuApp):
 
         wsgi.registory['RestCall'] = self.data
         path = '/gprs'
-
+         
         uri = path + '/info/{opt}'
         mapper.connect('stats',uri,
                        controller=RestCall, action='info',
                        conditions=dict(method=['GET']))
 
+        #volanie na uri /gprs/pdp/{add/mod/del} spusti funkciu mod_pdp v triede RestCall
         uri = path + '/pdp/{cmd}'
         mapper.connect('stats',uri,
                        controller=RestCall, action='mod_pdp',
                        conditions=dict(method=['POST']))
         
-#    def add_pdp_context(self, dp, pdp, tunnel):
-#        """ Add new flow rule to redirect user traffic into tunnel interface.
-#        
-#        Keyword arguments:
-#            dp -- datapath
-#            pdp -- pdp context
-#            tunnel -- outgoing interface number
-#
-#        """
-#
-#        print "add new OF rule to DP="+str(dp)
-#
-#        ofp = dp.ofproto
-#        parser = dp.ofproto_parser
-#
-# pouzivatelsky packet s danymi parametrami tlacime do dodaneho tunelu
-#        match = parser.OFPMatch(
-#                        in_port=BSS_PHY_PORT,
-#                        ns_type=0, 
-#                        ns_bvci=pdp.bvci,
-#                        bssgp_tlli=pdp.tlli,
-#                        llc_sapi=pdp.sapi,
-#                        sndcp_nsapi=pdp.nsapi)
-#        actions = [ GPRSActionPopGPRSNS(), GPRSActionPopUDPIP() , parser.OFPActionOutput(port=tunnel) ]
-#        inst = [ parser.OFPInstructionActions(ofp.OFPIT_APPLY_ACTIONS, actions) ]
-#        req = parser.OFPFlowMod(datapath=dp, table_id=OF_GPRS_TABLE, priority=10, match=match, instructions=inst);
-#        dp.send_msg(req)
-#
-#		# v opacnom smere nam vypadavaju z tunelu nejake packety.. balime ich do gprsns a posielame na bss
-#        # TODO: (mato) prepisat tento match, malo by sa matchovat:
-#        #  - zdrojova a cielova mac adresa tunela
-#        #  - cielova IP adresa (podla adresy ktoru ma priradeny PDP kontext)
-#        match = parser.OFPMatch( 
-#                in_port=TUNNEL_PHY_PORT,
-#                eth_type=0x0800, 
-#                eth_src=pdp.tunnel_internet,
-#                eth_dst=pdp.tunnel_bss,
-#                ipv4_dst=pdp.ip)
-#       # TODO: (mato)
-#       # adresa 10.11.12.13  adresa VGSN
-#       # adresa 20.21.22.23 je adresa BSS 
-#       # sp by mal byt port ktory sa nejako dozvieme (z prveho pripojenia BSS na VGSN)
-#       # dp by mal byt port na ktorom pocuva BSS, tiez sa ho dozvieme z prveho pripojenia BSS
-#      #XXX: kontrolny traffic z BSS na VGSN by sa mal asi posielat aj do controlleru, na analyzu cisiel portov?? minimalne na zaciatku
-#        actions = [
-#            GPRSActionPushGPRSNS( bvci=pdp.bvci, tlli=pdp.tlli, sapi=pdp.sapi, nsapi=pdp.nsapi),
-#            GPRSActionPushUDPIP( sa=VGSN_IP, da=BSS_IP, sp=VGSN_PORT, dp=BSS_PORT),
-#            parser.OFPActionOutput( port=BSS_PHY_PORT ) ] 
-#        inst = [ parser.OFPInstructionActions(ofp.OFPIT_APPLY_ACTIONS, actions) ]
-#        req = parser.OFPFlowMod(datapath=dp, table_id=OF_GPRS_TABLE, priority=11, match=match, instructions=inst);
-#        dp.send_msg(req)
-
     def on_inner_dp_join(self, dp):
         """ Add new inner (BSS side) forwarder joined our network.
         
@@ -294,12 +242,6 @@ class GPRSControll(app_manager.RyuApp):
             inst = [ parser.OFPInstructionActions(ofp.OFPIT_APPLY_ACTIONS, actions) ]
             req = parser.OFPFlowMod(datapath=dp, table_id=OF_GPRS_TABLE, priority=20, match=match, instructions=inst)
             dp.send_msg(req)
-    
-            # konkretne user packety posunieme, kam ich treba
-            #XXX: pravidla pridava funkcia add_pdp_context
-            #TODO: pridat vsetky aktivne PDP kontexty
-            #for pdp in self.active_contexts:
-            #    self.add_pdp_context(dp, pdp, TUNNEL_PHY_PORT)
     
            # ak to ma sndcp, su to user data neznameho PDP kontextu - drop
             match = parser.OFPMatch( sndcp_first_segment=1 )
@@ -343,12 +285,12 @@ class GPRSControll(app_manager.RyuApp):
     def vypadok(self, ev):
         print('~~~~~~~~~~~~~DEBUG~~~~~~~~~~~~~~~~~~~~~~~~~~')
 
+
 class RestCall(ControllerBase):
     def __init__(self, req, link, data, **config):
         super(RestCall, self).__init__(req, link, data, **config)
         self.dpset = data['dpset']
         self.waiters = data['waiters']
-        self.topo = topology()
         self.id_pool = []
 
     def info(self, req, opt):
@@ -356,7 +298,7 @@ class RestCall(ControllerBase):
         LOG.debug('~~~DEBUG~~~')
         return (Response(content_type='text', body=resp))
 
-    def mod_pdp (self, rest_body, cmd, mirror = 0, mirrorTID=0, TID=0):
+    def mod_pdp (self, rest_body, cmd, mirror = 0, TID=0, mirrorTID=0):
         #vytiahneme parametre z tela REST spravy
         body = eval(rest_body.body)
         start = int(body.get('start'), 16)
@@ -365,65 +307,81 @@ class RestCall(ControllerBase):
         tlli = int(body.get('tlli'), 16)
         sapi = int(body.get('sapi'))
         nsapi = int(body.get('nsapi'))
+        clientIP = str(body.get('clientIP'))
         #active_contexts.append( PDPContext(bvci, tlli, sapi, nsapi,'10.10.10.10', ) )
         two_way = body.get('mirror')
         if mirror == 0:
             mirrorTID = self.get_mac()
             TID = self.get_mac()
-        #vytvorime si instanciu triedy topology ktora ma graf siete
-        #TODO: zvazit ci tuto instanciu radsej nevytvarat v __init__() triedy aby bola spolocna a nevytvarala sa nova pri kazdom volani mod_tunnel()
-        topo = self.topo
-        # do 't' dostaneme tunnel triedy 'tunnels' (na zaciatku)
-        # v pripade ze mirror==1 (volanie mod_tunnel() pre vytvorenie tunela v opacnom smere) dostaneme spiatocny tunel 
-        t = topo.get_tunnel(start, end, TID, mirrorTID)
-        
-        # premennej cmd priradime OFP konstantu na zaklade adresy REST volania /stats/tunnels/{cmd} 
-        # zatial sa nepouziva, vzdy sa robi add
-        dp = self.dpset.get(t.nodes[1].dpid)
-        if cmd == 'add':
-            self.cmd = dp.ofproto.OFPFC_ADD
-        elif cmd == 'modify':
-            self.cmd = dp.ofproto.OFPFC_MODIFY
-        elif cmd == 'delete':
-            self.cmd = dp.ofproto.OFPFC_DELETE
-        else:
-            return Response(status=404)
-        
-        ##########Tato cast je pre prvy node v tunely pretoze ten musi vykonat aj zmenu MAC adresy################
-        parser = dp.ofproto_parser
-        ofp = dp.ofproto
-   
-        match = parser.OFPMatch( in_port=1,
-                                 ns_type=0,
-                                 ns_bvci=bvci, 
-                                 bssgp_tlli=tlli, 
-                                 llc_sapi=sapi, 
-                                 sndcp_nsapi=nsapi)
+        # do 't' dostaneme tunnel triedy 'tunnels'
+        #pri tunely semrom von start -> end
+        #pri tunely smerom dnu end -> start
+        if mirror==0:
+            t = topo.get_tunnel(start, end, TID, mirrorTID)
+        elif mirror==1:
+            t = topo.get_tunnel(end, start, TID, mirrorTID)
+
+        #TODO: Handlovanie 'cmd' hodnoty
+
+        ############################################Smerom von##############################################################################
+        if mirror==0:
+            ######Tato cast je pre prvy node v tunely smerom von, pretoze ten musi decapsulovat GPRS-NS aj zmenit MAC adresy################
+            dp = self.dpset.get(t.nodes[0].dpid)
+            parser = dp.ofproto_parser
+            match = parser.OFPMatch( in_port=1,
+                                     ns_type=0,
+                                     ns_bvci=bvci, 
+                                     bssgp_tlli=tlli, 
+                                     llc_sapi=sapi, 
+                                     sndcp_nsapi=nsapi)
                                
-        actions = [GPRSActionPopGPRSNS(), GPRSActionPopUDPIP(),
-                   parser.OFPActionSetField(eth_src=t.mirrorTID),parser.OFPActionSetField(eth_dst=t.TID),
-                   parser.OFPActionOutput(t.nodes[1].port_out)]
-        #inst = [ parser.OFPInstructionActions(ofp.OFPIT_APPLY_ACTIONS, actions) ]
-        #req = parser.OFPFlowMod(datapath=dp, match = match, table_id=OF_GPRS_TABLE, priority=10, instructions=inst)
-        #dp.send_msg(req) 
-        self.add_flow(dp, 10, match, actions, OF_GPRS_TABLE)
+            actions = [GPRSActionPopGPRSNS(), GPRSActionPopUDPIP(),
+                       parser.OFPActionSetField(eth_src=t.mirrorTID),parser.OFPActionSetField(eth_dst=t.TID),
+                       parser.OFPActionOutput(t.nodes[0].port_out)]
+            self.add_flow(dp, 10, match, actions, OF_GPRS_TABLE)
 
-        ##########################################################################################################
+            ###############################################################################################################################
 
-
-        #########Tento cyklus prebehne vsetky ostatne nody kde sa pridaju len forwardovacie pravidla##############
-        match = parser.OFPMatch(eth_dst=t.TID)
-        for var in t.nodes[2:]:
-            dp = self.dpset.get(var.dpid)
-            actions = [parser.OFPActionOutput(var.port_out)]
-            self.add_flow(dp, 10, match, actions, 0)
-        ##########################################################################################################
-
+            #########Tento cyklus prebehne pre vsetky ostatne nody kde sa pridaju len forwardovacie pravidla a na poslednom sa setne broadcast eth_dst#
+            for var in t.nodes[1:]:
+                dp = self.dpset.get(var.dpid)
+                parser = dp.ofproto_parser
+                match = parser.OFPMatch(eth_dst=t.TID)
+                actions = [parser.OFPActionOutput(var.port_out)]
+                if var.dpid == 0xc:
+                    actions.insert(0,parser.OFPActionSetField(eth_dst='ff:ff:ff:ff:ff:ff'))
+                self.add_flow(dp, 10, match, actions, 0)
+            ###############################################################################################################################
+        
+        ###################################Smerom dnu######################################################################################
+        if mirror==1:
+            dp = self.dpset.get(t.nodes[0].dpid)
+            parser = dp.ofproto_parser
+            ######################################Prvy paket na zaklade cielovej IP adresy (clientIP) natlacit do tunelu###################
+            match = parser.OFPMatch(eth_type=0x0800, ipv4_src=clientIP) 
+            actions = [parser.OFPActionSetField(eth_src=mirrorTID), parser.OFPActionSetField(eth_dst=TID),
+                       parser.OFPActionOutput(t.nodes[0].port_out)]
+            self.add_flow(dp, 9, match, actions, 0)
+            
+            #########Tento cyklus prebehne pre vsetky ostatne nody kde sa pridaju len forwardovacie pravidla a na poslednom sa pushnu GPRS signalizacne veci#
+            for var in t.nodes[1:]:
+                dp = self.dpset.get(var.dpid)
+                parser = dp.ofproto_parser
+                match = parser.OFPMatch(eth_dst=t.TID)
+                actions = [parser.OFPActionOutput(var.port_out)]
+                if var.dpid == 0xa:
+                    actions.insert(0, parser.OFPActionSetField(eth_dst='ff:ff:ff:ff:ff:ff'))
+                    actions.insert(0, GPRSActionPushUDPIP(sa=VGSN_IP, da=BSS_IP, sp=VGSN_PORT, dp=BSS_PORT))
+                    actions.insert(0, GPRSActionPushGPRSNS(bvci, tlli, sapi, nsapi)) 
+                self.add_flow(dp, 9, match, actions, 0)
+            ###############################################################################################################################
+            
+        
         # Ak pride v RESTE 'mirror' : 'yes' na zaciatku sa to ulozi do premennej 'two_way'
         # kontrola na mirror == 0 zabezpecuje aby sa nerekurzovalo donekonecna lebo rekurzivne zavolana funkcia ma mirror == 1
-        #if two_way == 'yes' and mirror == 0:     
-        #    LOG.debug('~~~~~~~~~~~~~~~~~~~~~~~~~~Debug~rekurs~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')  
-        #    self.mod_pdp(rest_body, cmd, 1, mirrorTID, TID)  
+        if two_way == 'yes' and mirror == 0:     
+            self.mod_pdp(rest_body, cmd, 1, mirrorTID, TID)   
+            LOG.debug('~~~~~~~~~~~~~~~~~~~~~~~~~~Debug~rekurs~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')  
         return (Response(content_type='text', body='ok'))
 
     def add_flow(self, dp, priority, match, actions, table = 0):
@@ -434,7 +392,7 @@ class RestCall(ControllerBase):
         mod = parser.OFPFlowMod(datapath=dp, priority=priority, table_id=table, match=match, instructions=inst)
         dp.send_msg(mod)
 
-    
+    #generator mac adresy pre ID tunela    
     def get_mac(self):
         mac_char='0123456789abcdef'
         mac_addr=''
