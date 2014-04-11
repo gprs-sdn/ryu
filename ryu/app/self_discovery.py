9af28200fd0eab2ec0b21a4f7e2a49621af0bbb0
kalcok <martin.kalcok@gmail.com>
2014-04-11 11:08:04 +0200

    Now it works :)

--- ryu/app/self_discovery.py
+++ ryu/app/self_discovery.py
@@ -11,6 +11,9 @@ from ryu.lib.packet import ethernet
 from ryu.lib.packet import icmp
 from ryu.lib.packet import ipv4
 import inspect
+import array
+import json
+import ast
 
 class Hello(app_manager.RyuApp):
     def __init__(self, *args, **kwargs):
@@ -18,7 +21,7 @@ class Hello(app_manager.RyuApp):
         switchList = []
         self.dpset=dpset
          
-    def _ping(self, dp):
+    def _ping(self, dp, port_out):
         ofp = dp.ofproto
         parser = dp.ofproto_parser
         pkt = packet.Packet()
@@ -33,17 +36,23 @@ class Hello(app_manager.RyuApp):
         pkt.add_protocol(icmp.icmp(type_=8,
                                    code=0,
                                    csum=0,
-                                   data=icmp.echo(1,1,str(dp.id))))
+                                   data=icmp.echo(1,1,"{'dpid' : "+str(dp.id)+",'port_out' : "+str(port_out)+"}")))
         pkt.serialize()
         data=pkt.data
-        actions=[parser.OFPActionOutput(1,0)]
+        actions=[parser.OFPActionOutput(port_out,0)]
         out=parser.OFPPacketOut(datapath=dp, buffer_id=ofp.OFP_NO_BUFFER, in_port=ofp.OFPP_CONTROLLER, actions=actions, data=data)
         dp.send_msg(out)
-        print('Floodujem z fw: ', dp.id)
+
     @set_ev_cls(ofp_event.EventOFPPacketIn)
     def _packet_in(self, ev):
       
-        print('Som',ev.msg.datapath.id,'a donstal som spravu na porte: ',ev.msg.match['in_port'])
+        nieghbourDPID=self._get_icmp_data(packet.Packet(array.array('B', ev.msg.data)),'dpid')
+        neighbourPort=self._get_icmp_data(packet.Packet(array.array('B', ev.msg.data)),'port_out')
+        print('Som',ev.msg.datapath.id,
+              ', dostal som spravu na porte: ',ev.msg.match['in_port'], 
+              ' od cisla:',nieghbourDPID,
+              ' ktory ma ma pripojeneho na porte',neighbourPort)
+        #print(ev.msg.data)
 
     @set_ev_cls(ofp_event.EventOFPStateChange)
     def switch_woke_up(self, ev):
@@ -61,19 +70,24 @@ class Hello(app_manager.RyuApp):
         self.add_flow(dp, 100, match, actions) 
 
         if ev.state == handler.MAIN_DISPATCHER:
-            if dp.id==12:
-                self._ping(dp)
-                for var in dp.ports:
-                    print(var)
+            #print('som up ',dp.id)
+            #self._ping(dp,1)
+            for var in dp.ports:
+                if var != (ofp.OFPP_CONTROLLER+1):
+                    self._ping(dp,var)
+                    #print(var, ofp.OFPP_CONTROLLER)
                 #print('~~~~~~~~~~~~~~~~~~debug~~~~~~~~~~~~~~~~~~~~~~~~~~~~', inspect.getmembers(dp))
 
-    def send_packet_out(self, datapath, buffer_id,  port_in, out_port):
-        ofp = datapath.ofproto
-        ofp_parser = datapath.ofproto_parser
-        print('skusil som')
-        actions = [ofp_parser.OFPActionOutput(ofp.OFPP_FLOOD, 0)]
-        req = ofp_parser.OFPPacketOut(datapath, ofp.OFP_NO_BUFFER, ofp.OFPP_CONTROLLER, actions)
-        datapath.send_msg(req)
+    def _get_icmp_data(self, pkt, req):
+
+        payload = ''
+        for p in pkt:
+            if p.protocol_name == 'icmp':
+                for char in p.data.data:
+                    payload+=(chr(char))
+        slovnik = ast.literal_eval(payload.rstrip('\0'))
+        return(slovnik.get(req))
+
 
 #  @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
 #  def _packet_in_handler(self, ev):
